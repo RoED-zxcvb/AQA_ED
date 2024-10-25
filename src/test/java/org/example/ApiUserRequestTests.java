d0c48aa903b5020b9329890449b23513e146de8d
Eduard <v-epm-eduardgimad@racetrac.com>
2024-10-25 13:51:00 +0300

    update tests for allure

--- src/test/java/org/example/ApiUserRequestTests.java
+++ src/test/java/org/example/ApiUserRequestTests.java
@@ -1,49 +1,74 @@
 package org.example;
 
+import io.qameta.allure.Step;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.complexAQA.utils.Properties;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 /**
- * TestClass for GET response
- * Libraries: API call with RestAssured
+ * Test class for validating API responses for user requests
  */
 public class ApiUserRequestTests {
 
-    private Response response;
-
-    @BeforeEach
-    public void getResponse() {
-        RequestSpecification userGetRequest = RestAssured
-                .given()
-                .baseUri(Properties.getHost() + Properties.getUserUrl() + 1);
-
-        response = userGetRequest.get();
-    }
-
 
     /**
-     * Test GET response
-     * Libraries: RestAssured assert
+     * Tests GET response status code
      */
     @Test
-    public void statusCodeTestWithRestAssured() {
-        response.then().statusCode(200);
+    public void testStatusCode() {
+        Response response = getUserResponseById(1);
+        validateResponseStatusCode(response, 200);
     }
 
 
     /**
      * Test response body contain correct user ID
-     * Libraries: Junit assert
      */
     @Test
-    public void responseAttributeTestWithRestAssuredAndJunitAssert() {
-        assertEquals(1, response.jsonPath().getInt("data.id"));
+    public void testUserIdInResponse() {
+        Response response = getUserResponseById(1);
+        validateUserIdInResponse(response, 1);
+    }
+
+    /**
+     * Sends a GET request to fetch a user by user ID.
+     *
+     * @param userID the ID of the user to retrieve
+     * @return the HTTP response containing the user data
+     */
+    @Step("Get user response by user ID={userID}")
+    public Response getUserResponseById(int userID) {
+        RequestSpecification userGetRequest = RestAssured
+                .given()
+                .baseUri(Properties.getHost() + Properties.getUserUrl() + userID);
+
+        return userGetRequest.get();
+    }
+
+    /**
+     * Validates that the user ID in the response body matches the expected ID.
+     *
+     * @param response the HTTP response to validate
+     * @param userID the expected user ID
+     */
+    @Step("Validate user ID in response matches expected ID={userID}")
+    public void validateUserIdInResponse(Response response, int userID) {
+        assertEquals(userID, response.jsonPath().getInt("data.id"));
+    }
+
+    /**
+     * Validates that the response status code matches the expected status code.
+     *
+     * @param response the HTTP response to validate
+     * @param statusCode the expected status code
+     */
+    @Step("Validate response status code matches expected code={statusCode}")
+    public void validateResponseStatusCode(Response response, int statusCode) {
+        response.then().statusCode(statusCode);
     }
 }
